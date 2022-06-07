dbda49bcd02ac61e090b4385952ee8e2a91ed628
Ran Wahle <ran.wahle@MBP-RAN.WAHLE>
2022-06-07 23:36:21 +0300

    todos-start

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Now from './Now';
+import TaskComponent from './task';
+
+const tasks = [
+  {title: 'title', description: 'description', dueDate: new Date()},
+  {title: 'title', description: 'description', dueDate: new Date()},
+  {title: 'title', description: 'description', dueDate: new Date()}
+]
 
 class App extends React.Component {
   render() {
     return (<>
-    <h1>Hello Melio</h1>
-    <Now now={new Date()}></Now>
+    <h1>Hello Melio, these are your tasks</h1>
+    {tasks.map(task => <TaskComponent {...task} ></TaskComponent>)}
     </>)
   }
 }
